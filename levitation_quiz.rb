0875b2b9b47b12a401d632dddd5e01da5d2e6c39
Lillian Zhang <lilyzhangyun@gmail.com>
2018-05-22 00:59:29 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -5,17 +5,7 @@ def levitation_quiz
       answer = gets.chomp
       break if answer == 'Wingardium Leviosa'
     end 
-
-	#your code here
+    puts 'You passed the quiz!'
 end
 
-def annoying
-    loop do 
-        puts "What's up, Doc?"
-        answer = gets.chomp
-        break if answer == "STOP"
-    end
-    puts "Okay, okay, jeez. I'll stop. Sorry."
-end
- 
-annoying
+levitation_quiz
